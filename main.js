500b2b09cfc606a86a4411eca062dfba3062852e
Treasure Ajefu <treasureajefu@gmail.com>
2022-09-08 14:18:19 +0100

    finished mobile navigation

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-import { CountUp } from 'countUp.js';
+let body = document.getElementById('body')
+let menu = document.getElementById('menu')
+let menuBar = document.getElementById('hamburger')
+let close = document.getElementById('close-icon')
+let menuItems = document.getElementsByClassName('menu-items')
 
-window.onload = function() {
-  var countUp = new CountUp('countries', 2000);
-  countUp.start();
+function openNavbar() {
+    body.classList.add('overflow-hidden')
+    menu.classList.add('-right-8')
+    menu.classList.remove('right-[100vh]')
+
 }
+function closeNavbar() {
+    menu.classList.remove('-right-8')
+    menu.classList.add('right-[100vh]')
+    body.classList.remove('overflow-hidden')
+}
+
+menuBar.addEventListener("click", openNavbar)
+close.addEventListener("click", closeNavbar)
